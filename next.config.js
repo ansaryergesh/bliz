baa5dfb7b1b6aee6a1862f58e44e3c4e049a7def
ansaryergesh <yergeshovansar@gmail.com>
2021-05-25 10:29:15 +0600

    google map api changed

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ module.exports = {
   env: {
     BASE_URL: 'https://test.money-men.kz/api',
     DOC_URL: 'https://test.money-men.kz/',
-    GOOGLE_MAP_API_KEY: 'AIzaSyAplKiP9AOLuUbWdH655ApFMl1nnfXwcwk',
-    PROD_URL: 'https://bliz-kz.herokuapp.com',
+    GOOGLE_MAP_API_KEY: 'AIzaSyDwgq_3mUoJkLm7aTUbH2Z44y6RVm92DV0',
+    PROD_URL: 'https://bliz.vercel.app/',
     apiKey: 'AIzaSyCYG4nyVXnKBg6ap2IhzGIM-Ks44moiV0o',
     authDomain: "bliz-6af36.firebaseapp.com",
     databaseURL: "https://bliz-6af36-default-rtdb.firebaseio.com",
