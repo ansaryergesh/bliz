ddb285946167d10154f8cacef945b71d8a0b4f79
Ansar Yergeshov <yergeshovansar@gmail.com>
2021-09-17 11:08:31 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-  env: {
-    BASE_URL: 'https://test.money-men.kz/api',
-    DOC_URL: 'https://test.money-men.kz/',
-    GOOGLE_MAP_API_KEY: 'AIzaSyDwgq_3mUoJkLm7aTUbH2Z44y6RVm92DV0',
-    PROD_URL: 'https://bliz.vercel.app/',
-    apiKey: 'AIzaSyCYG4nyVXnKBg6ap2IhzGIM-Ks44moiV0o',
-    authDomain: "bliz-6af36.firebaseapp.com",
-    databaseURL: "https://bliz-6af36-default-rtdb.firebaseio.com",
-    projectId: "bliz-6af36",
-    storageBucket: "bliz-6af36.appspot.com",
-    messagingSenderId: "970557461414",
-    appId: "1:970557461414:web:3d3c7d0db11636bee53567",
-    measurementId: "G-9L8W7T2CL0"
-  },
-  trailingSlash: true,
-}
+
